8d6be92b5d697ea94c465fef9519f2368f7b8e2b
ufo123456 <64509749+ufo123456@users.noreply.github.com>
2020-12-05 17:46:20 +0800

    Add files via upload

--- test1.c
+++ test1.c
@@ -7,13 +7,12 @@ int main()
 	
 
 
-	double foot;
-    double inch;
+	double foot;//定义输入 
+    double inch;//...... 
 	
-	scanf("%lf %lf",&foot, &inch);
-	
+	scanf("%lf %lf",&foot, &inch);//接收输入 
 	printf("身高是%f米.\n",
-	((foot+ inch / 12) * 0.3048));
+	((foot+ inch / 12) * 0.3048));//计算打印 
 	return 0;
 
 
