bde2d50fcc382805b005af3c99263b491810dae5
ufo123456 <64509749+ufo123456@users.noreply.github.com>
2020-12-05 16:34:53 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int price = 0;
+	
+   printf("«Î ‰»ÎΩ∂Ó£®yan£©");
+   scanf("%d",&price);
+   
+   int change = 100 - price;
+   
+   print("100 - change");
+   return 0;
+}
